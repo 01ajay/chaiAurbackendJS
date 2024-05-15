d26e6124568704576fba433ba0536b405cb1dd89
ajay <ajaykumarg@integramicro.com>
2024-05-15 16:38:14 +0530

    auth verification middleware  route updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,8 +4,27 @@ import User from "../models/user.model.js";
 import { uploadCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshToken = async (userID) => {
+  try {
+    const user = await User.findById(userID);
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while genrating refresh token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
-  /*algorithm for user register */ 
+  /*algorithm for user register */
   //get user details frontend
   //validation
   //- not empty
@@ -16,8 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
   //remove password and refresh token field from response
   //check for user creation
   // return res
-  
-  
+
   //console.log("whole req=>", req.body);
   //console.log("req-files=>", req.files);
   const { username, fullname, email, password } = req.body;
@@ -85,4 +103,87 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, userCreated, "User Register Successfully."));
 });
 
-export { registerUser };
+const userLogin = asyncHandler(async (req, res) => {
+  /*algorithm for user login*/
+  //1 extract the username or email,password from req
+  //2 validate the field for empty
+  //3 check user is present or not
+  //4 check password matches or not
+  //5 access token and refresh token
+  //6 send secure cookies
+
+  const { email, username, password } = req.body;
+
+  if (!email || !username) {
+    throw new ApiError(400, "username or password is required");
+  }
+
+  /*Note - this "User"  is monogo DB but whenever you 
+            want to access own method that will be 
+            accessible "userPresent"*/
+
+  const userPresent = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!userPresent) {
+    throw new ApiError(400, "User doesn't exist");
+  }
+  const isPasswordValid = await userPresent.isPasswordCorrect(password);
+  if (isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+  const [accessToken, refreshToken] = await generateAccessAndRefreshToken(
+    userPresent._id
+  );
+
+  const loggedInUser = User.findById(userPresent._id).select(
+    "-password -refreshToken"
+  );
+
+  //cookiest option object
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .send(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+export const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  res
+    .send(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+export { registerUser, userLogin };
