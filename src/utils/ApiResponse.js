fcedb25494613abdbacd204bd815a261498432f0
ajay <ajaykumarg@integramicro.com>
2024-05-15 18:30:20 +0530

    comment removed and working till logout

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,7 +1,6 @@
 class ApiResponse {
   constructor(statusCode, data, message = "Success") {
     this.statusCode = statusCode;
-    console.log("Data=>",data);
     this.data = data;
     this.message = message;
     this.success = statusCode < 100;
